5ead96a8338a999a3080ee8b31b8c196eced1ce5
tung <tbt@gmail.com>
2024-01-10 08:53:17 +0700

    add file model 1

---
+++ thucHanh1.java
@@ -0,0 +1,21 @@
+public class thucHanh1 {
+    public static void main(String[] args) {
+        // bài 1
+        System.out.println("hello word");
+        // bài 2
+        String c = "hello word";
+        System.out.println(c);
+        // bài 3
+        String a = "hello";
+        String b = "word";
+        System.out.println(a + " " + b);
+        // bài4
+        String d = inWord("word");
+        System.out.println(a + " " + d);
+
+    }
+
+    public static String inWord(String d) {
+        return d;
+    }
+}
